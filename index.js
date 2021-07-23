4cbbde803dabb39735fa7adcae288d38765090c7
iman <imsofos@gmail.com>
2021-07-23 19:26:42 +0430

    handle variable in url + handle request body + handle querystrings + routhing using regex

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const PORT = process.env.PORT || 10000
 const { createServer } = require('http')
-const { router } = require('./router');
+const { router } = require('./router')
 
-createServer(router).listen(PORT, () => {
+createServer(async (request, response) => {
+    console.log(new Date(), request.method, request.url)
+    await router(request, response)
+}).listen(PORT, () => {
     console.log(`Listening at *:${PORT}`)
 });
--- router.js
+++ router.js
@@ -1,16 +1,51 @@
-const { indexController, notFoundController } = require('./controllers');
+const { indexController, notFoundController, serveFile } = require('./controllers')
+const { pathToRegexp } = require("path-to-regexp");
+const querystring = require('querystring');
 
-const routeTable = {
-    '/': indexController
-};
 
-const router = (request, response) => {
-    const url = request.url
-    if (typeof routeTable[url] === 'function') {
-        routeTable[url](request, response)
-    } else {
-        notFoundController(request, response)
+function dispather(routeTableInput) {
+    const routeTable = new Map();
+    const routes = Object.keys(routeTableInput);
+    for (let i = 0; i < routes.length; i++) {
+        const route = routes[i];
+        const temp = route.split(' ')
+        const method = temp[0]
+        if (!routeTable.has(method)) routeTable.set(method, new Map());
+        const keys = [];
+        routeTable.get(method).set(pathToRegexp(temp[1], keys), { keys, controller: routeTableInput[route] })
+    }
+    return async (request, response) => {
+        const myUrl = request.url.split('?')
+        const methodRoutes = routeTable.get(request.method);
+        if (!methodRoutes) return notFoundController(request, response)
+        const routes = methodRoutes.keys()
+        for (let i = 0; i < methodRoutes.size; i++) {
+            const route = routes.next().value;
+            const exec = route.exec(myUrl[0]);
+            if (!exec) continue
+            const { keys, controller } = methodRoutes.get(route);
+            for (let j = 0; j < keys.length; j++) request[keys[j].name] = exec[j + 1]
+            request.params = {...querystring.parse(myUrl[1])};
+            request.body = '';
+            request.on('data', (data) => {
+                request.body += data;
+            });
+            request.on('end', () => {
+                try {
+                    request.body = JSON.parse(request.body)
+                } catch (err) {}
+                return controller(request, response)
+            });
+        }
     }
 }
 
-module.exports = { router };
+const routeTable = {
+    'GET /': indexController,
+    'POST /:id': indexController,
+    'GET /public/:filePath(.*)': serveFile
+}
+
+module.exports = {
+    router: dispather(routeTable)
+}
